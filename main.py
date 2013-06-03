78abcf0febce7947c7b78f850d7892b61b4739e4
MARTA <ltreed@itsmarta.com>
2013-06-03 11:24:29 -0400

    changed variable 'type' to 'mode' to be more accurate

--- main.py
+++ main.py
@@ -21,28 +21,28 @@ def main():
 def usage():
 	print("\n\n--help\n\nThis is how you use it.\n\n")
 
-def getInput(type):
+def getInput(mode):
 	vehicle = ""
-	if (type == "route"):
+	if (mode == "route"):
 		vehicle = "Buses"
 	else:
 		vehicle = "Trains"
-	message = '\n\n*******Get' + vehicle + '*******\n\nPlease enter a route number\n\n(leave blank for all routes)\n\n(type \'?\' for a list of ' + type + 's)\n\n'
+	message = '\n\n*******Get' + vehicle + '*******\n\nPlease enter a route number\n\n(leave blank for all routes)\n\n(type \'?\' for a list of ' + mode + 's)\n\n'
 	
 	userInput = input(message)
 
 	if (userInput == "?"):
-		showJSON(type)
-		userInput = getInput(type)
+		showJSON(mode)
+		userInput = getInput(mode)
 
 	return userInput
 	
-def showJSON(type):
+def showJSON(mode):
 	json_data = open('json')
 
 	data = json.load(json_data)
-	print("\n\nList of " + type + "s:\n")
-	for i in data[type]:
+	print("\n\nList of " + mode + "s:\n")
+	for i in data[mode]:
 		print(i)
 	json_data.close()
 
